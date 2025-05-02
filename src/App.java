5397c97042fb167f0367b25578dbf06dad067aec
IturielLSaenz <ilsaenzacademic@gmail.com>
2025-05-02 15:03:31 -0600

    GUI - implemented
    Working GUI!

--- src/App.java
+++ src/App.java
@@ -1,15 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        /*
-        Aquí irá el código para la GUI
-        
-        */ 
-        //creando un objeto:
-        Person persona = new Person("Ituriel","8713488381","iliebes@anahuac.mx");
-        Date fecha = new Date(2025,4,10);
-        Time timeStart = new Time(10,0,0);
-        Time timeEnd = new Time(11,30,0);
-        Event evento1 = new Event("Clase programación","Clase para aprender a programar",persona,fecha,timeStart,timeEnd);
-        System.out.println(evento1.toString());
+    public static void main(String[] args) {
+        // Crear contacto principal (el dueño de la agenda)
+        Person owner = new Person("Ituriel", "8713488381", "iliebes@anahuac.mx");
+
+        // Crear agenda y agregar un evento opcional
+        Agenda agenda = new Agenda(owner);
+
+        // Lista compartida de contactos
+        List<Person> contacts = new ArrayList<>();
+        contacts.add(owner); // Puedes iniciar con uno o más contactos
+
+        // Crear GUI en el hilo de la interfaz
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new AgendaForm(agenda, contacts).setVisible(true);
+        });
     }
 }
--- src/ContactForm.java
+++ src/ContactForm.java
@@ -2,20 +2,40 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+import java.util.List;
+import javax.swing.DefaultListModel;
 
 /**
  *
  * @author ituriel_liebes
  */
 public class ContactForm extends javax.swing.JFrame {
-
+    private List<Person> contacts; 
     /**
      * Creates new form ContactForm
      */
-    public ContactForm() {
+    
+    public ContactForm(java.util.List<Person> contacts) {
         initComponents();
+        this.contacts = contacts;
+        actualizarLista();
+        jButton2.setEnabled(false);
+        
+        // tras jList1.setModel(modelo);
+        jList1.addListSelectionListener(evt -> {
+            // Solo actuamos cuando la selección se haya “fijado”
+            if (!evt.getValueIsAdjusting()) {
+                jButton2.setEnabled(jList1.getSelectedIndex() != -1);
+            }
+        });
     }
-
+    private void actualizarLista() {
+        DefaultListModel<String> modelo = new DefaultListModel<>();
+        for (Person p : contacts) {
+            modelo.addElement(p._toString());
+        }
+        jList1.setModel(modelo);
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -58,6 +78,11 @@ public class ContactForm extends javax.swing.JFrame {
         jLabel2.setText("Nombre:");
 
         nombre.setText("nombre");
+        nombre.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                nombreActionPerformed(evt);
+            }
+        });
 
         jLabel3.setText("Tel:");
 
@@ -79,15 +104,9 @@ public class ContactForm extends javax.swing.JFrame {
             }
         });
 
-        jList1.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
         jScrollPane1.setViewportView(jList1);
 
         jButton2.setText("Eliminar contacto");
-        jButton2.setEnabled(false);
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
@@ -129,7 +148,7 @@ public class ContactForm extends javax.swing.JFrame {
                                 .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                             .addComponent(jButton3))
                         .addGap(18, 18, 18)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 174, Short.MAX_VALUE)))
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 341, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -166,54 +185,48 @@ public class ContactForm extends javax.swing.JFrame {
 
     private void telefonoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_telefonoActionPerformed
         // TODO add your handling code here:
+        
     }//GEN-LAST:event_telefonoActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        String nombreTexto = nombre.getText();
+        String telTexto = telefono.getText();
+        String emailTexto = email.getText();
+
+        if (!nombreTexto.isEmpty() && !telTexto.isEmpty() && !emailTexto.isEmpty()) {
+            Person nuevo = new Person(nombreTexto, telTexto, emailTexto);
+            contacts.add(nuevo);
+            actualizarLista();
+            nombre.setText("");
+            telefono.setText("");
+            email.setText("");
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        int idx = jList1.getSelectedIndex();
+        if (idx >= 0) {
+            contacts.remove(idx);
+            actualizarLista();
+            // Después de borrar, no hay selección
+            jButton2.setEnabled(false);
+        }
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void nombreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nombreActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_nombreActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ContactForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ContactForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ContactForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ContactForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ContactForm().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField email;
